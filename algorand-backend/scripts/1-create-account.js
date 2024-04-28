0c3c3465c0438a8a1056add3657be5ee7bc73a94
Samarth Sharma <116581050+SAMARTH-SHARMA3@users.noreply.github.com>
2024-04-28 13:24:08 +0000

    installing algorand

---
+++ algorand-backend/scripts/1-create-account.js
@@ -0,0 +1,7 @@
+import algosdks from "algosdk";
+
+const myaccount = algosdks.generateAccount();
+console.log("Account created. Save address & Mnemonic");
+console.log("Account Address =" + myaccount.addr);
+let account_mnemonic = algosdk.secretKeyToMnemonic(myaccount.sk);
+console.log("Account Mnemonic =" + account_mnemonic);
