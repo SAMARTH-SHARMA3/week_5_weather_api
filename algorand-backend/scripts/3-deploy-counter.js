c39b9f7539eb0c47e4d2a81c9b863e9d93228ecd
Samarth Sharma <116581050+SAMARTH-SHARMA3@users.noreply.github.com>
2024-04-29 13:23:40 +0000

    creating 3-deploy-counter.js

--- algorand-backend/scripts/3-deploy-counter.js
+++ algorand-backend/scripts/3-deploy-counter.js
@@ -1,66 +1,86 @@
 import dotenv from "dotenv";
 import algosdk from "algosdk";
 import { open, readFile } from "node:fs/promises";
-
 dotenv.config();
 
 const baseServer = "https://testnet-api.algonode.cloud";
-const alogodClient = new algosdk.Algodv2("", baseServer, "");
 
-let myaccount = algosdk.mnemonicToSecretKey("excite empty daring robust leg affair fly old anger swap debate slide walk goddess curious amount piano faint awake utility key bounce reopen able put");
+const algodClient = new algosdk.Algodv2("", baseServer, "");
 
+let myaccount = algosdk.mnemonicToSecretKey(
+  "marble bargain devote struggle black rain ticket meadow couple until soup catalog jar afraid helmet scare legal cinnamon profit craft proof tuna elegant absorb insect"
+);
 let sender = myaccount.addr;
 
-async function compileProgram(client, TealSource){
-    let encoder = new TextEncoder();
-    let programBytes = encoder.encode(TealSource);
-    let compileResponse = await client.compile(programBytes).do();
-    let compiledBytes = new Uint8Array(Buffer.from(compileResponse.result, "base64"));
-    return compiledBytes;
+async function compileProgram(client, TealSource) {
+  let encoder = new TextEncoder();
+  let programBytes = encoder.encode(TealSource);
+  let compileResponse = await client.compile(programBytes).do();
+  let compiledBytes = new Uint8Array(
+    Buffer.from(compileResponse.result, "base64")
+  );
+  return compiledBytes;
 }
 
-(async() =>{
-    try{
-        const localInts = 0
-        const localBytes = 0
-        const globalInts = 1
-        const globalBytes = 0
+(async () => {
+  try {
+    const localInts = 0;
+    const localBytes = 0;
+    const globalInts = 1;
+    const globalBytes = 0;
 
-        let approvalProgramfile = await open('./contracts/artifacts/counter_approval.teal');
-        let clearProgramfile = await open('./contracts/artifacts/counter_clear.teal');
+    let approvalProgramfile = await open(
+      "./contracts/artifacts/counter_approval.teal"
+    );
+    let clearProgramfile = await open(
+      "./contracts/artifacts/counter_clear.teal"
+    );
 
-        const approvalProgram = await approvalProgramfile.readFile();
-        const clearProgram = await clearProgramfile.readFile();
+    const approvalProgram = await approvalProgramfile.readFile();
+    const clearProgram = await clearProgramfile.readFile();
 
-        const approvalProgramBinary = await compileProgram(algodClient, approvalProgram);
-        const clearProgramBinary = await compileProgram(algodClient, clearProgram);
+    const approvalProgramBinary = await compileProgram(
+      algodClient,
+      approvalProgram
+    );
+    const clearProgramBinary = await compileProgram(algodClient, clearProgram);
 
-        let params = await algodClient.getTransactionParams().do();
-        const onComplete = algosdk.OnApplicationComplete.NoOpOC;
+    let params = await algodClient.getTransactionParams().do();
+    const onComplete = algosdk.OnApplicationComplete.NoOpOC;
 
-        console.log("Deploying APPlication.  .  .  .  .  .  .  .  ")
+    console.log("Deploying Application. . . . ");
 
-        let txn = algosdk.makeApplicationCreateTxn(sender, params, onComplete, approvalProgramBinary, clearProgramBinary, localInts, localBytes, globalInts, globalBytes,);
+    let txn = algosdk.makeApplicationCreateTxn(
+      sender,
+      params,
+      onComplete,
+      approvalProgramBinary,
+      clearProgramBinary,
+      localInts,
+      localBytes,
+      globalInts,
+      globalBytes
+    );
+    let txId = txn.txID().toString();
 
-        let txId = txn.txID().toString();
-        
+    // Sign the transaction
+    let signedTxn = txn.signTxn(myaccount.sk);
+    console.log("Signed transaction with txID: %s", txId);
 
-        let signedTxn = txn.signTxn(myaccount.sk);
-        console.log("Signed transaction with txID: %s", txId);
+    // Submit the transaction
+    await algodClient.sendRawTransaction(signedTxn).do();
 
-        await algodClient.sendRawTransaction(signedTxn).do();
+    // Wait for confirmation
+    await algosdk.waitForConfirmation(algodClient, txId, 2);
 
-        await algosdk.waitForConfirmation(algodClient, txId, 2);
-
-        let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
-        let appId = transactionResponse['application-index'];
-
-        console.log("Created new with app-id: ", appId);
-
-    }
-
-    catch(err){
-        console.error("Failed to deploy!", err);
-        process.exit(1);
-    }
-})
+    // print the app-id
+    let transactionResponse = await algodClient
+      .pendingTransactionInformation(txId)
+      .do();
+    let appId = transactionResponse["application-index"];
+    console.log("Created new with app-id: ", appId);
+  } catch (err) {
+    console.error("Failed to deploy!", err);
+    process.exit(1);
+  }
+})();
